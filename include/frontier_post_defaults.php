7d9d6acc59399446093729a7a3a11c68747b24f3
finnj <finnj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-19 08:21:52 +0000

    v 3.0.2
    git-svn-id: https://plugins.svn.wordpress.org/frontier-post/tags/3.0.2@1048983 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/frontier_post_defaults.php
+++ include/frontier_post_defaults.php
@@ -12,8 +12,9 @@ $default_post_edit_max_age		= 7;
 $default_post_delete_max_age	= 3;
 
 $frontier_option_list 	= array('can_add', 'can_edit', 'can_publish', 'can_private', 'can_draft', 'can_delete',  'redir_edit', 'show_admin_bar', 'exerpt_edit', 'tags_edit',  'can_media', 'editor', 'category', 'default_category');
+$frontier_cap_list 		= array('frontier_post_can_add', 'frontier_post_can_edit', 'frontier_post_can_publish', 'frontier_post_can_private', 'frontier_post_can_draft', 'frontier_post_can_delete', 'frontier_post_redir_edit', 'frontier_post_show_admin_bar', 'frontier_post_exerpt_edit', 'frontier_post_tags_edit', 'frontier_post_can_media');
 $editor_types 			= array(__('Full Editor', 'frontier-post') => 'full', __('Minimal Visual', 'frontier-post') => 'minimal-visual', __('Minimal-Html', 'frontier-post') => 'minimal-html', __('Text Only', 'frontier-post') => 'text');
-$category_types 		= array(__('Multi select', 'frontier-post') => 'multi', __('Checkbox', 'frontier-post') => 'checkbox', __('Single select', 'frontier-post') => 'single', __('Hide', 'frontier-post') => 'hide');
+$category_types 		= array(__('Multi select', 'frontier-post') => 'multi', __('Checkbox', 'frontier-post') => 'checkbox', __('Single select', 'frontier-post') => 'single', __('Hide', 'frontier-post') => 'hide', __('Read only', 'frontier-post') => 'readonly');
 $frontier_option_slice 	= 8;
 
 $std_mce_buttons_1		= 'bold, italic, strikethrough, bullist, numlist, blockquote, justifyleft, justifycenter, justifyright, link, unlink, wp_more, fullscreen, wp_adv';
@@ -28,6 +29,8 @@ $frontier_mce_buttons_4 = '';
 
 $frontier_author_default_caps = array('delete_posts' => true, 'delete_published_posts' => true, 'edit_posts' => true, 'edit_published_posts' => true, 'publish_posts' => true, 'private_posts' => false, 'read' => true, 'upload_files' => true);
 $frontier_author_role_name	  = "frontier-author";
+
+$frontier_default_submit = array('save' => 'true', 'savereturn' => 'true', 'preview' => 'true', 'cancel' => 'true' );
 
 
 ?>
